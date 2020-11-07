3f8e4e62eda97821bcdac5c47cbcc51950905715
vinikaushik <69781514+vinikaushik@users.noreply.github.com>
2020-11-07 12:06:47 -0600

    Add files via upload

---
+++ src/main/java/com/OnlineInventory/Order/ItemDetails.java
@@ -0,0 +1,201 @@
+package com.OnlineInventory.Order.Response;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+
+public class ItemDetails {
+	
+    // order specific
+    private Integer itemStatus;
+    private Double itemTotalAmount;
+    private Integer quantity;
+    private Double itemPrice;
+    private Double taxAmount;
+    private Integer discountType;
+    private String discountCode;
+    private Double discountAmount;
+    private String itemComment;
+    private String createdBy;
+    private Timestamp createDate;
+    private Integer updatedBy;
+    private Timestamp lastUpdate;
+	
+	// Item specific
+	private Long itemId;
+    private  String itemName;
+    private Integer productId;
+    private String sku;
+    private Long busyItemCode;
+    private String longName;
+    private String description;
+    private Double price;
+    private String itemColor;
+    private String itemSize;
+    
+    // produst detail
+    
+    private String productName;
+	private String productLongName;
+	private String prodDescription;
+    
+    
+	public Long getItemId() {
+		return itemId;
+	}
+	public void setItemId(Long itemId) {
+		this.itemId = itemId;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public Integer getProductId() {
+		return productId;
+	}
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	public String getSku() {
+		return sku;
+	}
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	public Long getBusyItemCode() {
+		return busyItemCode;
+	}
+	public void setBusyItemCode(Long busyItemCode) {
+		this.busyItemCode = busyItemCode;
+	}
+	public String getLongName() {
+		return longName;
+	}
+	public void setLongName(String longName) {
+		this.longName = longName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public String getItemColor() {
+		return itemColor;
+	}
+	public void setItemColor(String itemColor) {
+		this.itemColor = itemColor;
+	}
+	public String getItemSize() {
+		return itemSize;
+	}
+	public void setItemSize(String itemSize) {
+		this.itemSize = itemSize;
+	}
+	public Integer getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(Integer updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	public Integer getItemStatus() {
+		return itemStatus;
+	}
+	public void setItemStatus(Integer itemStatus) {
+		this.itemStatus = itemStatus;
+	}
+	public Double getItemTotalAmount() {
+		return itemTotalAmount;
+	}
+	public void setItemTotalAmount(Double itemTotalAmount) {
+		this.itemTotalAmount = itemTotalAmount;
+	}
+	public Double getItemPrice() {
+		return itemPrice;
+	}
+	public void setItemPrice(Double itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+	public Double getTaxAmount() {
+		return taxAmount;
+	}
+	public void setTaxAmount(Double taxAmount) {
+		this.taxAmount = taxAmount;
+	}
+	public Integer getDiscountType() {
+		return discountType;
+	}
+	public void setDiscountType(Integer discountType) {
+		this.discountType = discountType;
+	}
+	public String getDiscountCode() {
+		return discountCode;
+	}
+	public void setDiscountCode(String discountCode) {
+		this.discountCode = discountCode;
+	}
+	public Double getDiscountAmount() {
+		return discountAmount;
+	}
+	public void setDiscountAmount(Double discountAmount) {
+		this.discountAmount = discountAmount;
+	}
+	public String getItemComment() {
+		return itemComment;
+	}
+	public void setItemComment(String itemComment) {
+		this.itemComment = itemComment;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Timestamp getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Timestamp createDate) {
+		this.createDate = createDate;
+	}
+	public Timestamp getLastUpdate() {
+		return lastUpdate;
+	}
+	public void setLastUpdate(Timestamp lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductLongName() {
+		return productLongName;
+	}
+	public void setProductLongName(String productLongName) {
+		this.productLongName = productLongName;
+	}
+	public String getProdDescription() {
+		return prodDescription;
+	}
+	public void setProdDescription(String prodDescription) {
+		this.prodDescription = prodDescription;
+	}
+    
+	
+}
